490ead05e7edc26cdefe0160e6f61be9138c2d6e
dww <dwward@gmail.com>
2021-01-20 09:27:19 -0500

    Adding preloading of townlands csv

--- app.py
+++ app.py
@@ -4,19 +4,59 @@ from io import BytesIO, StringIO
 from werkzeug import FileWrapper, secure_filename
 import simplekml
 import csv
+import pandas as pd
+
 app = Flask(__name__)
+
+
+def load_townland_data():
+    filename = 'townlands-no-geom-0907202-0810.csv'
+    # Townland fields of interest
+    # OSM Dict: OSM_ID(0), NAME_TAG(1), NAME_ENG(3), PARISH(13), COUNTY(10)
+    # datatype = {'ATTRIBUTIO': string}
+    # OSM_ID, NAME_TAG, NAME_GA, NAME_EN, ALT_NAME, ALT_NAME_G, OSM_USER, OSM_TIMEST, ATTRIBUTIO, LOGAINM_RE, CO_NAME, CO_OSM_ID, CO_LOGAINM, CP_NAME, CP_OSM_ID, CP_LOGAINM, ED_NAME, ED_OSM_ID, ED_LOGAINM, BAR_NAME, BAR_OSM_ID, BAR_LOGAIN, T_IE_URL, AREA, LATITUDE, LONGITUDE, EPOCH_TSTM
+    # int, string, string, string, string, string, string, datetime, string, int, string, float
+
+    pd.set_option('display.max_columns', None)
+    towndata = pd.read_csv(filename, dtype={8: 'string', 9: 'string'})
+    return towndata
+
+
+app.townland = load_townland_data()
 
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    f = request.files['fileToUpload']
-    csvf = StringIO(f.read().decode())
-    people = csv.reader(csvf, delimiter=',')
-    for row in people:
-        print(row)
-    # print(f.readline())
+    # Parse townlands data
+    # f2 = request.files['file2']
+    # f2 = StringIO(f2.read().decode())
+    # f2_csv = csv.reader(f2, delimiter=',')
+    # fields = next(f2_csv)
+    # print(fields)
+
+    # Parse file1 upload
+    f1 = request.files['file1']
+    f1 = StringIO(f1.read().decode())
+    f1_csv = csv.reader(f1, delimiter=',')
+    fields = next(f1_csv)
+
+    for row in f1_csv:
+        townland = row[2]
+        parish = row[3]
+        county = row[4]
+        first_name = row[1]
+        last_name = row[0]
+
+        df = app.townland
+        # loc = df[(df["CO_NAME"] == county) & (df["CP_NAME"] == parish)]
+        loc = df[((df["NAME_TAG"] == townland) | (df["NAME_EN"] == townland))
+                 & (df["CP_NAME"] == parish)
+                 & (df["CO_NAME"] == county)]
+        if loc.empty:
+            print(first_name + ' ' + last_name + ' (' + county + ', ' + parish + ", " + townland + ')')
+
     kml = simplekml.Kml()
-    pnt = kml.newpoint(name="A Point")
+    pnt = kml.newpoint(name="A Point")  # f1-2 f1-1 f1-7
     """
     // county
     // townland
